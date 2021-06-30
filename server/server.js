090e2a609255eb0802a5be7e7021c1b2d1742624
bluesatyr <shawnevans.music@gmail.com>
2021-06-30 15:03:50 -0400

    create seed file and user-routes.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const userRoutes = require("./routes/user-routes.js");
+
+// express middleware, used to be body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// app.use(require('./routes'));
+app.use("/api/", userRoutes);
+
+// start api server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
